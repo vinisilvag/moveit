f72658ae86dbde44b3b1515888a9e4a3ffe00523
Vinicius Gomes <vinicius.svgomes@gmail.com>
2021-02-27 15:29:19 -0300

    Codigo da aula 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengeContext'
+
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/58532241?s=460&u=db2c83598aa06aadd59ba4497f205c70934cbd68&v=4" alt="Vinícius Gomes"/>
 
       <div>
         <strong>Vinícius Gomes</strong>
-        <p><img src="icons/level.svg" alt="Level"/>Level 1</p>
+        <p><img src="icons/level.svg" alt="Level"/>Level {level}</p>
       </div>
     </div>
   );
